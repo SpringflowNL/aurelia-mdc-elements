22a834dd32dc13423b87f3bbc2b434f6fdd73b82
jvalst <jvalst@springflow.nl>
2018-04-11 09:26:26 +0200

    FIXED details issue (double trigger)

--- dist/commonjs/components/details/details.js
+++ dist/commonjs/components/details/details.js
@@ -13,24 +13,32 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var DetailsIe = exports.DetailsIe = (_dec = (0, _aureliaFramework.customAttribute)('details-ie'), _dec2 = (0, _aureliaFramework.inject)(Element), _dec(_class = _dec2(_class = function () {
     function DetailsIe(element) {
+        var _this = this;
+
         _classCallCheck(this, DetailsIe);
 
         this.element = element;
+
+        this.handleClick = function (e) {
+            if (_this.element.getAttribute("open") == "") {
+                _this.element.removeAttribute("open");
+            } else {
+                _this.element.setAttribute("open", "");
+            }
+        };
     }
 
-    DetailsIe.prototype.bind = function bind() {
+    DetailsIe.prototype.attached = function attached() {
         var ua = window.navigator.userAgent;
         var msie = ua.indexOf("Trident") + window.navigator.userAgent.indexOf("Edge") != -2;
+        debugger;
         if (msie) {
-            var elem = this.element;
-            this.element.children[0].addEventListener("click", function () {
-                if (elem.getAttribute("open") == "") {
-                    elem.removeAttribute("open");
-                } else {
-                    elem.setAttribute("open", "");
-                }
-            });
+            this.element.children[0].addEventListener("click", this.handleClick);
         }
+    };
+
+    DetailsIe.prototype.detached = function detached() {
+        this.element.children[0].removeEventListener("click", this.handleClick);
     };
 
     return DetailsIe;
