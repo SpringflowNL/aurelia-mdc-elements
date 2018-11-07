bb3d995bf1ef72e4ef0b5b29d5517c48f500695c
jvalst <jvalst@springflow.nl>
2018-11-07 08:29:44 +0100

    ADDED node_modules to sass paths ADDED autoprefixer for css/scss

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -2,10 +2,15 @@ import gulp from 'gulp';
 import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import autoprefixer from 'gulp-autoprefixer';
 
 export default function processCSS() {
-  return gulp.src(project.cssProcessor.source)
-    .pipe(changedInPlace({firstPass: true}))
+  return gulp.src(project.cssProcessor.source)	
+  	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(changedInPlace({firstPass: true}))
     .pipe(build.bundle());
 }
 
--- aurelia_project/tasks/process-scss.js
+++ aurelia_project/tasks/process-scss.js
@@ -3,10 +3,17 @@ import sass from 'gulp-sass';
 import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import autoprefixer from 'gulp-autoprefixer';
 
 export default function processSCSS() {
   return gulp.src(project.scssProcessor.source)
-    .pipe(sass().on('error', sass.logError))    
+    .pipe(sass({
+		includePaths: ['node_modules']
+	  }).on('error', sass.logError))    
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
     .pipe(changedInPlace({firstPass: true}))
     .pipe(build.bundle());
 }
