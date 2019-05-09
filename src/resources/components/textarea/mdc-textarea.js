785eb3dd8857a6b75eb32bac4410216c93476ea2
jvalst <jvalst@springflow.nl>
2019-05-09 13:04:57 +0200

    FIXED mdc textarea structure to mdc 2.1.0

--- src/resources/components/textarea/mdc-textarea.js
+++ src/resources/components/textarea/mdc-textarea.js
@@ -4,36 +4,41 @@ import { MDCTextField } from '@material/textfield/dist/mdc.textfield.min';
 @customElement('mdc-textarea')
 @inject(Element)
 export class MdcTextarea {
-	@bindable label;
-	@bindable({defaultBindingMode:bindingMode.twoWay}) value;
-	@bindable disabled = false;
-	@bindable required = false;
-	@bindable modifier = null;
-	@bindable secondarylabel = null;
+    @bindable label;
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) value;
+    @bindable disabled = false;
+    @bindable required = false;
+    @bindable modifier = null;
+    @bindable secondarylabel = null;
 
-	constructor ( element)
-	{
-		this.element = element;
-		this.unique = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-	}
+    constructor(element) {
+        this.element = element;
+        this.unique = (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
 
-	bind()
-	{
-		this.myMdcTextfield = new MDCTextField(this.element.firstElementChild);
-		this.myMdcTextfield.disabled = this.disabled;
-		this.myMdcTextfield.required = this.required;
-	}
+    bind() {
+        this.myMdcTextfield = new MDCTextField(this.element.firstElementChild);
+        this.myMdcTextfield.disabled = this.disabled;
+        this.myMdcTextfield.required = this.required;
 
-	disabledChanged(newvalue) {
-		this.myMdcTextfield.disabled = newvalue;
-	}
+        if (this.value) {
+            this.myMdcTextfield.value = this.value;
+        }
+    }
 
-	requiredChanged(newvalue) {
-		this.myMdcTextfield.required = newvalue;
-	}
+    valueChanged(newvalue) {
+        this.myMdcTextfield.value = newvalue;
+    }
 
-	detached()
-	{
-		this.myMdcTextfield.destroy();
-	}
+    disabledChanged(newvalue) {
+        this.myMdcTextfield.disabled = newvalue;
+    }
+
+    requiredChanged(newvalue) {
+        this.myMdcTextfield.required = newvalue;
+    }
+
+    detached() {
+        this.myMdcTextfield.destroy();
+    }
 }
