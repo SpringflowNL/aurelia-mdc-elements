e306c580110b6406bbc5935980a9a605a92eb6dc
kvdheuvel <kvdheuvel@springflow.nl>
2019-05-10 11:27:09 +0200

    EDIT mdc-checkbox structure and styling changes FIXED mdc-text-field outlined styling

--- src/resources/components/checkbox/mdc-checkbox.js
+++ src/resources/components/checkbox/mdc-checkbox.js
@@ -12,7 +12,7 @@ export class MdcCheckbox {
 	@bindable model;
 	@bindable secondarylabel;
 
-    constructor( element) {
+    constructor(element) {
         this.element= element;
         this.unique = (((1+Math.random())*0x10000)|0).toString(16).substring(1);
     }
