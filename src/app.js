5b752f151f419882cfa9510be1fe56329123832d
jvalst <jvalst@springflow.nl>
2019-05-13 14:03:00 +0200

    FIXED app.html dynamic array

--- src/app.js
+++ src/app.js
@@ -7,72 +7,76 @@ import { MDCTabBar } from '@material/tab-bar/dist/mdc.tabBar';
 
 @inject(EventAggregator)
 export class App {
-  constructor(ea) {
-    this.ea = ea;
+    constructor(ea) {
+        this.ea = ea;
 
-    this.label = 'Custom';
-    this.type = 'text';
-    this.modifier = null;
-    this.leading = null;
-    this.trailing = null;
-  }
+        this.label = 'Custom';
+        this.type = 'text';
+        this.modifier = null;
+        this.leading = null;
+        this.trailing = null;
+        this.staticArray = [
+            { key: true, value: 'True story' },
+            { key: false, value: 'Bad story' }
+        ];
+    }
 
-  prefillValues() {
-	this.value = 'Value';
-    this.boolvalue = true;
-    this.secondary = 'Helper text';
+    prefillValues() {
+        this.dynamicArray = [
+            { key: true, value: 'True story' },
+            { key: false, value: 'Bad story' }
+        ];
 
-    this.testArray = [
-		{ key: true, value: 'True story' },
-		{ key: false, value: 'Bad story' }
-	];
-  }
+        this.value = 'Value';
+        this.boolvalue = true;
+        this.secondary = 'Helper text';
+    }
 
-  attached() {
-    this.topAppBar = new MDCTopAppBar(
-      document.querySelector('.mdc-top-app-bar')
-    );
-    this.drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
+    attached() {
+        this.topAppBar = new MDCTopAppBar(
+            document.querySelector('.mdc-top-app-bar')
+        );
+        this.drawer = new MDCDrawer(document.querySelector('.mdc-drawer'));
 
-    this.slider = new MDCSlider(this.slider);
-    this.sliderDisabled = new MDCSlider(this.sliderDisabled);
-    this.sliderDiscrete = new MDCSlider(this.sliderDiscrete);
-    this.sliderDiscreteDisabled = new MDCSlider(this.sliderDiscreteDisabled);
-    this.slidertick = new MDCSlider(this.sliderTick);
-    this.slidertickDisabled = new MDCSlider(this.sliderTickDisabled);
+        this.slider = new MDCSlider(this.slider);
+        this.sliderDisabled = new MDCSlider(this.sliderDisabled);
+        this.sliderDiscrete = new MDCSlider(this.sliderDiscrete);
+        this.sliderDiscreteDisabled = new MDCSlider(this.sliderDiscreteDisabled);
+        this.slidertick = new MDCSlider(this.sliderTick);
+        this.slidertickDisabled = new MDCSlider(this.sliderTickDisabled);
 
-    this.tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+        this.tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
-    this.handleTopAppBarWithDrawer();
-    this.handleSliders();
-  }
+        this.handleTopAppBarWithDrawer();
+        this.handleSliders();
+    }
 
-  handleTopAppBarWithDrawer() {
-    this.topAppBar.setScrollTarget(document.getElementById('main-content'));
-    this.topAppBar.listen('MDCTopAppBar:nav', () => {
-      this.drawer.open = !this.drawer.open;
-    });
-  }
+    handleTopAppBarWithDrawer() {
+        this.topAppBar.setScrollTarget(document.getElementById('main-content'));
+        this.topAppBar.listen('MDCTopAppBar:nav', () => {
+            this.drawer.open = !this.drawer.open;
+        });
+    }
 
-  handleSliders() {
-    const self = this; 
+    handleSliders() {
+        const self = this;
 
-    setTimeout(function() {
-      self.slider.layout();
-      self.sliderDisabled.layout();
-      self.sliderDiscrete.layout();
-      self.sliderDiscreteDisabled.layout();
-      self.slidertick.layout();
-      self.slidertickDisabled.layout();
-    }, 200);
+        setTimeout(function() {
+            self.slider.layout();
+            self.sliderDisabled.layout();
+            self.sliderDiscrete.layout();
+            self.sliderDiscreteDisabled.layout();
+            self.slidertick.layout();
+            self.slidertickDisabled.layout();
+        }, 200);
 
-    return true;
-  }
+        return true;
+    }
 
-  triggerSnackbar() {
-    this.ea.publish('PostMessage.Snackbar', {
-      label: this.label,
-      buttonlabel: this.secondary
-    });
-  }
+    triggerSnackbar() {
+        this.ea.publish('PostMessage.Snackbar', {
+            label: this.label,
+            buttonlabel: this.secondary
+        });
+    }
 }
